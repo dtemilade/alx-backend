49e0cc98c019e22f7f674f6800ad32dcc72da346
dtemilade <dtemilade@gmail.com>
2024-05-06 17:49:53 +0100

    Updating commits for task accomplished

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -48,26 +48,25 @@ class Server:
             return []
         return self.dataset()[start:end]
 
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+        """method that takes the same arguments (and defaults)
 
-def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
-    """method that takes the same arguments (and defaults)
+            Args:
+                page (int, optional): . Defaults to 1.
+                page_size (int, optional): . Defaults to 10.
 
-        Args:
-            page (int, optional): . Defaults to 1.
-            page_size (int, optional): . Defaults to 10.
-
-        Returns:
-            Dict[str, Any]:
-    """
-    total_pages = math.ceil(len(self.dataset()) / page_size)
-    return {
-            "page_size": page_size,
-            "page": page,
-            "data": self.get_page(page, page_size),
-            "next_page": page + 1 if page + 1 <= total_pages else None,
-            "prev_page": page - 1 if page > 1 else None,
-            "total_pages": total_pages
-            }
+            Returns:
+                Dict[str, Any]:
+        """
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        return {
+                "page_size": page_size,
+                "page": page,
+                "data": self.get_page(page, page_size),
+                "next_page": page + 1 if page + 1 <= total_pages else None,
+                "prev_page": page - 1 if page > 1 else None,
+                "total_pages": total_pages
+               }
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
